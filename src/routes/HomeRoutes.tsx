793a6f71ac172d29c249d3dad9169f5500a0c701
SyedFaizAlhady SyedAhmadAlhady <SyedFaizAlhady_SyedAhmadAlhady@jabil.com>
2025-10-22 00:46:35 +0800

    22/10

--- src/routes/HomeRoutes.tsx
+++ src/routes/HomeRoutes.tsx
@@ -1,9 +1,12 @@
+// import DashboardPage from "@/pages/DashboardPage";
 import { lazy } from "react";
 import type { RouteObject } from "react-router-dom";
 
-const HomePage = lazy(() => import("@/pages/home/HomePage"));
+const DashboardPage = lazy(() => import("@/pages/DashboardPage"));
+// const HomePage = lazy(() => import("@/pages/home/HomePage"));
 
 export const HomeRoute: RouteObject = {
   path: "/",
-  element: <HomePage />,
+  element: <DashboardPage />,
+  // element: <HomePage />,
 };
